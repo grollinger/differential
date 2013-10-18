0f9478e7e652df5d5ba1d2a8119b2e146afc73ff
Georg Rollinger <rthundr@live.de>
2013-10-18 18:00:09 -0300

    Rewrite Bruss2D to use Problem Interface

--- problems/bruss2d.go
+++ problems/bruss2d.go
@@ -1,10 +1,5 @@
 package problems
 
-type cell struct {
-	// Concentrations of the Reactants X and Y
-	u, v float64
-}
-
 type brusselator struct {
 	// Reaction Constants A and B
 	a, b float64
@@ -12,13 +7,13 @@ type brusselator struct {
 	alpha float64
 	// Grid Side length
 	n int
+	// Grid Cell Count for input validation
+	cellcount int
 
 	// Precalculated constants
 	// alphaN1Squared = alpha * (n - 1)^2
 	// a1 = A - 1
 	alphaN1Squared, a1 float64
-
-	concentration []cell
 }
 
 func v0(x, y float64) float64 {
@@ -29,27 +24,55 @@ func u0(x, y float64) float64 {
 	return 2 + 0.25*y
 }
 
-func (b *brusselator) Rate(index int) (du, dv float64) {
-	top, right, bottom, left := index-b.n, index+1, index+b.n, index-1
-	if top < 0 {
-		top = bottom
-	} else if bottom >= len(b.concentration) {
-		bottom = top
+func (b *brusselator) Initialize() (grid []float64) {
+	grid = make([]float64, b.cellcount*2)
+
+	n1 := float64(b.n) - 1.0
+
+	for i := range grid {
+		if i%2 == 1 {
+			continue
+		}
+
+		x, y := i%b.n, i/b.n
+		xNorm, yNorm := float64(x)/n1, float64(y)/n1
+		u, v := u0(xNorm, yNorm), v0(xNorm, yNorm)
+
+		grid[i] = u
+		grid[i+1] = v
 	}
-
-	if idxModN := index % b.n; idxModN == 0 {
-		left = right
-	} else if b.n-idxModN == 1 {
-		right = left
-	}
-
-	conc := b.concentration
-
-	du = b.b + conc[index].u*conc[index].u*conc[index].v - b.a1*conc[index].u + b.alphaN1Squared*(conc[top].u+conc[bottom].u+conc[left].u+conc[right].u-4.0*conc[index].u)
-
-	dv = b.a*conc[index].u - conc[index].u*conc[index].u*conc[index].v + b.alphaN1Squared*(conc[top].v+conc[bottom].v+conc[left].v+conc[right].v-4.0*conc[index].v)
-
 	return
+}
+
+func (b *brusselator) Fcn(t float64, yT []float64, dy_out []float64) {
+	// TODO validate input
+
+	for index := 0; index < b.cellcount; index++ {
+		top, right, bottom, left := index-b.n, index+1, index+b.n, index-1
+		if top < 0 {
+			top = bottom
+		} else if bottom >= b.cellcount {
+			bottom = top
+		}
+
+		if idxModN := index % b.n; idxModN == 0 {
+			left = right
+		} else if b.n-idxModN == 1 {
+			right = left
+		}
+		top <<= 1
+		bottom <<= 1
+		left <<= 1
+		right <<= 1
+		here := index << 1
+
+		// du = B + u^2*v - (A + 1)*u + (alpha * (n-1)^2) (u_top+u_bottom+u_left+u_right-4 * u)
+		dy_out[here] = b.b + yT[here]*yT[here]*yT[here+1] - b.a1*yT[here] + b.alphaN1Squared*(yT[top]+yT[bottom]+yT[left]+yT[right]-4.0*yT[here])
+
+		// dv = A * u - u^2*v + (alpha * (n-1)^2) * (v_top + v_bottom + v_left + v_right - 4 * v)
+		dy_out[here+1] = b.a*yT[here] - yT[here]*yT[here]*yT[here+1] + b.alphaN1Squared*(yT[top+1]+yT[bottom+1]+yT[left+1]+yT[right+1]-4.0*yT[here+1])
+
+	}
 }
 
 // New initializes a new Brusselator 2D problem
@@ -62,18 +85,9 @@ func (b *brusselator) Rate(index int) (du, dv float64) {
 func NewBruss2D(n int) (b brusselator) {
 
 	b.a, b.b, b.n = 3.4, 1.0, n
+	b.cellcount = n * n
 	b.alpha = 0.002
 	n1 := float64(n) - 1.0
 	b.a1, b.alphaN1Squared = b.a+1.0, b.alpha*n1*n1
-	b.concentration = make([]cell, n*n)
-
-	for i := range b.concentration {
-		x, y := i%n, i/n
-		xNorm, yNorm := float64(x)/n1, float64(y)/n1
-		u, v := u0(xNorm, yNorm), v0(xNorm, yNorm)
-
-		b.concentration[i] = cell{u, v}
-	}
-
 	return
 }
